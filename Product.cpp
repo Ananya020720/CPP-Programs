c5fbb1677f2ad46ddd43746c6ad057014f8a3ca5
Ananya020720 <83279685+Ananya020720@users.noreply.github.com>
2021-09-16 10:29:21 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    double a, b, pro;
+    cout << "Enter two numbers: "<< endl;
+    cin>>a>>b;
+    pro=a*b;
+    cout << "Product of the given two numbers is:" <<pro<<endl;
+
+    return 0;
+}
